0e2bd03153b3c80b1434d92790dedb41052403a0
Fernando Ginez da Silva <fginez@gmail.com>
2016-12-30 15:46:12 -0200

    dc
    Driver de caracters
    --fg, 30/12/2016

---
+++ dc/dc.c
@@ -0,0 +1,45 @@
+#include <linux/module.h>
+#include <linux/version.h>
+#include <linux/kernel.h>
+#include <linux/types.h>
+#include <linux/kdev_t.h>
+#include <linux/fs.h>
+ 
+static dev_t first; // Global variable for the first device number
+
+////////////////////////////////////////////////////////////////////////////////////
+// Duas funcoes diferentes da API do kernel podem ser chamadas para registrar o dev:
+//
+// int register_chrdev_region(dev_t first, unsigned int cnt, char *name);
+// -> ira alocar iniciando na primeira posicao de minor (a quantidade 'cnt')
+//
+// int alloc_chrdev_region(dev_t *first, unsigned int firstminor, unsigned int cnt, char *name);
+// -> ira alocar iniciando na posicao 'firstminor' (a quantidade 'cnt')
+
+
+// Neste código não é feita a criacao do 'device file', portanto, deve ser feito um mknod para
+// que o mesmo apareca em '/dev'
+
+static int __init ofcd_init(void) /* Constructor */
+{
+    printk(KERN_INFO "Namaskar: ofcd registered");
+    if (alloc_chrdev_region(&first, 0, 3, "Shweta") < 0)
+    {
+        return -1;
+    }
+    printk(KERN_INFO "<Major, Minor>: <%d, %d>\n", MAJOR(first), MINOR(first));
+    return 0;
+}
+ 
+static void __exit ofcd_exit(void) /* Destructor */
+{
+    unregister_chrdev_region(first, 3);
+    printk(KERN_INFO "Alvida: ofcd unregistered");
+}
+ 
+module_init(ofcd_init);
+module_exit(ofcd_exit);
+ 
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Anil Kumar Pugalia <email_at_sarika-pugs_dot_com>");
+MODULE_DESCRIPTION("Our First Character Driver");
